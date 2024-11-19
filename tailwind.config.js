488d1aaba31d8d18bc6c7febc8b4516d113add5c
Sangharsha <sangamtyata3@gmail.com>
2024-11-19 20:59:05 +0545

    modified the landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["*.{html,js}", "login.html"],
+
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        charcoal: "#2F3E46",
+        darkslategray: "#354F52",
+        hookergreen: "#52796F",
+        cambridgeblue: "#84A98C",
+        ashgray: "#CAD2C5",
+      },
+    },
   },
   plugins: [],
-}
+};
